d49c568303c79259ca50b6889bc1c06679e0eff3
Farmiana17 <blueruby0717@gmail.com>
2021-08-22 12:45:42 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,10 @@ class PostsController < ApplicationController
   #end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content])
+    #「post」キーに対して、「新規投稿したメモの内容」を格納する
+    render = json:{ post: post }
+    #「redirect_to action: :index」から変更
+    # → レスポンスをJSON形式で行い、「post」の「新規投稿したメモの内容」と「キー」をJavaScriptに送信
   end
 end
